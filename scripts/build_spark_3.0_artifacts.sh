eeb05cb75fa096c824df71e15dd233a109d8eadf
yl790 <yitao@rstudio.com>
2020-06-14 18:13:45 +0000

    build script
    Signed-off-by: yl790 <yitao@rstudio.com>

--- scripts/build_spark_3.0_artifacts.sh
+++ scripts/build_spark_3.0_artifacts.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sbt -Dversion='0.6.1-SNAPSHOT' -Dscala.version='2.12.10' -Dspark.version='3.0.0-preview2' assemblyNoTest
+sbt -Dversion='0.6.0' -Dscala.version='2.12.10' -Dspark.version='3.0.0-preview2' assemblyNoTest
