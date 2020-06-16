128ad5f60555cf3d5dabcc62a0829465f4b19646
yl790 <yitao@rstudio.com>
2020-06-16 14:32:36 +0000

    version bump
    Signed-off-by: yl790 <yitao@rstudio.com>

--- scripts/build_spark_2.4_artifacts.sh
+++ scripts/build_spark_2.4_artifacts.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sbt -Dversion='0.6.0' -Dscala.version='2.11.8' -Dspark.version='2.4.5' assemblyNoTest
+sbt -Dversion='0.6.2' -Dscala.version='2.11.8' -Dspark.version='2.4.5' assemblyNoTest
