8c0621be9161f9a0ebc04aa5d421f3ed18033da5
yl790 <yitao@rstudio.com>
2020-07-03 11:42:58 +0000

    also build the same artifacts with scala 2.12
    Signed-off-by: yl790 <yitao@rstudio.com>

--- scripts/publish_spark_2.4_artifacts.sh
+++ scripts/publish_spark_2.4_artifacts.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-sbt -Dversion='0.6.2' -Dscala.version='2.11.8' -Dspark.version='2.4.5' publish
+sbt -Dversion='0.7.0' -Dscala.version='2.11.8' -Dspark.version='2.4.5' publish
+sbt -Dversion='0.7.0' -Dscala.version='2.12.10' -Dspark.version='2.4.5' publish
